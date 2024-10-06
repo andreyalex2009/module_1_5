d756e6cabdc10d3a533f888c2ae176ab63610772
andreyalex2009 <andreyalex2009@yandex.ru>
2024-10-06 20:32:08 +0300

    Add files via upload

---
+++ module_1_5.py
@@ -0,0 +1,8 @@
+immutable_var = (1, 2, 'String', True)
+print(immutable_var)
+immutable_var[2] = 'Ура'
+print(immutable_var)
+mutable_list = [2, 4, 7, 9, 3]
+print(mutable_list)
+mutable_list[1] = 8
+print(mutable_list)
